7a66f7ef3439f207af08caba50f5977a51fffc03
aidatulkaromah@gmail.com <you@example.com>
2025-11-05 12:40:49 +0700

    Second Commit

--- StudiKasus1_NajwaAidatulKaromah_2505051.py
+++ StudiKasus1_NajwaAidatulKaromah_2505051.py
@@ -6,8 +6,8 @@
 print("===== Soal NO 1 =====")
 import numpy as np
 
-suhu_c = np.array([32, 45, 50, 39, 28, 22, 21, 33, 26, 34])
-suhu_f = (suhu_c * 9/5) + 32 
+suhu_c = np.array([27, 31, 34, 39, 28, 22, 21, 33, 26, 34]) #suhu dalam celcius
+suhu_f = (suhu_c * 9/5) + 32 #konversi suhu ke dalam fahrenheit
 
 print("Suhu dalam Celcius: ", suhu_c)
 print("Suhu dalam Fahrenheit: ", suhu_f)
